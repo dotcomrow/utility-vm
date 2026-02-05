d5e85f3fb16bbfe2a2b6dd532bb4363a7ed96494
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-02-05 11:52:55 -0500

    updating disk image
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,10 +18,8 @@ variable "admin_ssh_public_key" {
   type        = string
 }
 
-
-
 variable "vm_img" {
-  default = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+  default = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
   type    = string
 }
 
